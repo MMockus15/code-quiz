6f76fc2604eb665fc3ad2e38d8a29ddc77b7b18b
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-09-01 11:56:14 -0600

    attching css to html

---
+++ assets/js/highscores.js
@@ -0,0 +1,8 @@
+
+
+
+// query selector for go back
+// query selector for clear scores
+// event listener to clear score
+// for loop to record scores
+// event listener click go back button
---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+// view highscores: event listener for click to highscores page/ link highscores page
+// timer counting down when quiz start is clicked
+// timer to subtract time when there's a wrong answer
+
+// <h1 changes questions every x seconds
+// answers/buttons change along with questions
+// dataset for answer buttons 
+// right or wrong pop up using hide element
+
+
+// end of quiz form to enter initals / submit button
+// shows score with initals and then povides go back and clear high scores button
