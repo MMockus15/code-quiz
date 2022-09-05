d4bcf4a4f5fe2600f07cae2cf98e51f22a4a8235
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-09-05 12:49:08 -0600

    questions are listed

--- assets/js/mainlogic.js
+++ assets/js/mainlogic.js
@@ -7,11 +7,6 @@ var timerElement = document.querySelector("#quizTimer");
 var questionsContainer = document.getElementById("questions-screen");
 var checkAnswer = document.getElementById("answer-check");
 // var question = quizQuestions[index].question;
-var choiceA = document.getElementById("button-0");
-var choiceB = document.getElementById("button-1");
-var choiceC = document.getElementById("button-2");
-var choiceD = document.getElementById("button-3");
-var answerCheck = document.getElementById("answer-check");
 var questionsUl = document.getElementById("questionsUl");
 var choicesButtons = document.querySelectorAll(".choicesButtons");
 
@@ -21,63 +16,115 @@ var totalScore = 0;
 var score = 0;
 var quizQuestionsEmpty = 0;
 
+var quizQuestions = [
+  {
+    question: "What is NOT a JavaScript Data Type?",
+    choices: ["String", "Object", "inIt", "Boolean"],
+    answer: "inIt",
+  },
+  {
+    question: "If x = 25 and y = 50, which is true?",
+    choices: ["x = y", "x < y", "x > y ", "x++"],
+    answer: "x < y",
+  },
+  {
+    question: "An if / else statement is closed within:",
+    choices: ["Curly brackets", "Square brackets", "Quotes", "Parentheses"],
+    answer: "Parenthesis",
+  },
+  {
+    question: "The console.log is used for:",
+    choices: ["Event Listeners", "Debugging", "For Loops", "Datasets"],
+    answer: "debugging",
+  },
+  {
+    question: "What is an example of an eventListener triggering event?",
+    choices: ["Array", "Button", "Dataset", "Click"],
+    answer: "click",
+  },
+  {
+    question: "What are the types of popup boxes available in JavaScript?",
+    choices: ["Alert", "Confirm", "Prompt", "All of the above"],
+    answer: "All of the above",
+  },
+];
+
 // click start button and starts game
 startButton.addEventListener("click", startGame);
 
 // start game function
 function startGame() {
-	startTimer();
+  startTimer();
 }
 
 function startTimer() {
-	var timerInterval = setInterval(function () {
-		if (timerCount >= 0) {
-			timerElement.textContent = timerCount + " seconds left";
-			timerCount--;
-		} else {
-			clearInterval(timerInterval);
-		}
-	}, 1000);
-	
-	questionsStarting();
-	// render(quetionsUl);
+  var timerInterval = setInterval(function () {
+    if (timerCount >= 0) {
+      timerElement.textContent = timerCount + " seconds left";
+      timerCount--;
+    } else {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+
+  getQuestion();
+  // render(quetionsUl);
 }
 
-function questionsStarting() {
-	// first get current question object from array
-	var questionsContainer = quizQuestions[quizQuestionsEmpty];
+function getQuestion() {
+	// get current question object from array
+	var currentQuestion = quizQuestions[quizQuestionsEmpty];
+  
 	// update title with current question
-	
-	var questionsUl = document.getElementById("questionsUl");
-
-	var 
-	
-	questionsUl.textContent = questionsContainer.title;
-	// clear out any old question choices 
-	quizQuestions.innerHTML = "";
-	
-	// for loop thru questions
-	for (var i = 0; i < questionsContainer.choices.length; i++) {
-		
-		// create a button for each choice
-		var choice = quizQuestions.choices[i];
-		var answersBtns = document.createElement('button');
-		answersBtns.textContent = i + 1 + ". " + choice;
-		answersBtns.setAttribute('value', questionsContainer.options[i]);
-		// answersBtns.setAttribute('class', 'choice')
-		
-		// checks answer on click - need to set up function for choicesButtons
-		choicesButtons.eventListener("click", answerCheck);
-		
-		// display on the page
-		quizQuestions.appendChild(answersBtns);
+	var titleEl = document.getElementById('questionsUl');
+	titleEl.textContent = currentQuestion.title;
+  
+	console.log(questionsContainer);
+	// clear out any old question choices
+	questionsContainer.innerHTML = '';
+  
+	// loop over choices
+	for (var i = 0; i < currentQuestion.choices.length; i++) {
+	  // create new button for each choice
+	  var choice = currentQuestion.choices[i];
+	  var choiceNode = document.createElement('button');
+	  choiceNode.setAttribute('class', 'choice');
+	  choiceNode.setAttribute('value', choice);
+  
+	  choiceNode.textContent = i + 1 + '. ' + choice;
+  
+	  // display on the page
+	  questionsContainer.appendChild(choiceNode);
 	}
-	questionsContainer.append(answersBtns, questionsUl);
-}
+};
 
 
+
+
+
+
+
+
+//   for (var i = 0; i < 6; i++) {
+//     var choice = quizQuestions[i].choices[i];
+    
+//     var answersBtns = document.createElement("button");
+//     answersBtns.textContent = i + 1 + ". " + choice;
+
+//     console.log(choicesButtons);
+
+//     answersBtns.eventListener("click", answerCheck);
+
+//     // display on the page
+//     questionsContainer.appendChild(answersBtns);
+//   }
+//   questionsContainer.append(answersBtns, questionsUl);
+// }
+
+// function answerCheck() {
+//   console.log("hello");
+// }
 
 // starts timer when quiz start button is selected
 // when wrong answer take off 10 seconds
-// question++;
-
+// question++
